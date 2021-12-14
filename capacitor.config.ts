0696b00b6e8ae50de2dcaa2c6a1a3511b40e785c
Ikerls <ikerlarrea150@gmail.com>
2021-12-14 15:04:26 +0100

    build: agregado un proyecto basico de ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'city-ways',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

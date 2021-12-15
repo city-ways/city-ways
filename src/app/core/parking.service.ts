12fc6ecf42e0d2304c695e928fd7edc171055b22
Ikerls <ikerlarrea150@gmail.com>
2021-12-15 16:38:51 +0100

    refactor: refactorizado el servicio de parking

--- src/app/core/parking.service.ts
+++ src/app/core/parking.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Parking } from '../shared/parking';
-import { map, mapTo, reduce } from 'rxjs/operators';
+import { catchError, map, mapTo, reduce } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,58 +13,10 @@ export class ParkingService {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   private url = `${environment.apiUrlBase}/parkings`;
+
   constructor(private http: HttpClient) {}
 
-  public getParkings(): Observable<Parking[]> {
-    return this.executeQueryGet<Parking[]>('parkings');
-  }
-  public getMaxParkingId(): Observable<number> {
-    return this.executeQueryGet<Parking[]>('parkings').pipe(
-      map((data) =>
-        Math.max.apply(
-          Math,
-          data.map((parkingObj) => parkingObj.id)
-        )
-      )
-    );
-  }
-  public getParkingById(id: number): Observable<Parking> {
-    return this.executeQueryGet<Parking>('parkings', `/${id}`);
-  }
-  public createParking(parking: Parking): Observable<Parking> {
-    return this.executeQueryPost<Parking>('parkings', parking);
-  }
-  public deleteParking(id: number): Observable<any> {
-    return this.executeQueryDelete<any>('/parkings', id);
-  }
-  public updateParking(product: Parking): Observable<Parking> {
-    return this.http.put<Parking>(`${this.url}/${product.id}`, product, {
-      headers: this.headers,
-    });
-  }
-
-  private executeQueryGet<T>(endpoint: string, query?: string): Observable<T> {
-    // todo: intentar refacotizar la query
-    return this.http.get<T>(
-      `${environment.apiUrlBase}/${endpoint}${(query ??= '')}`
-    );
-  }
-  private executeQueryPost<T>(
-    endpoint: string,
-    newObj: Parking
-  ): Observable<T> {
-    return this.http.post<T>(`${environment.apiUrlBase}`, newObj, {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  private executeQueryDelete<T>(endpoint: string, id: number): Observable<T> {
-    return this.http.delete<T>(`${environment.apiUrlBase}/${id}`, {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  private handleError(err: any) {
+  private static handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
     let errorMessage: string;
@@ -78,5 +30,45 @@ export class ParkingService {
     }
     console.error(err);
     return throwError(errorMessage);
+  }
+
+  public getParkings(): Observable<Parking[]> {
+    return this.http
+      .get<Parking[]>(this.url)
+      .pipe(catchError(ParkingService.handleError));
+  }
+
+  public getMaxParkingId(): Observable<number> {
+    return this.http.get<Parking[]>(this.url).pipe(
+      map((data) =>
+        Math.max.apply(
+          Math,
+          data.map((parkingObj) => parkingObj.id)
+        )
+      ),
+      catchError(ParkingService.handleError)
+    );
+  }
+
+  public getParkingById(id: number): Observable<Parking> {
+    return this.http
+      .get<Parking>(`${this.url}/${id}`)
+      .pipe(catchError(ParkingService.handleError));
+  }
+
+  public createParking(parking: Parking): Observable<Parking> {
+    return this.http
+      .post<Parking>(this.url, parking, {
+        headers: this.headers,
+      })
+      .pipe(catchError(ParkingService.handleError));
+  }
+
+  public deleteParking(id: number): Observable<any> {
+    return this.http
+      .delete<any>(`${this.url}/${id}`, {
+        headers: this.headers,
+      })
+      .pipe(catchError(ParkingService.handleError));
   }
 }

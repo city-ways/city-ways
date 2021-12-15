d9cf13ea4b0627598e849dbc3211206360424ea0
iker <ikerls@pm.me>
2021-12-15 11:05:10 +0100

    feat: agregado en el servicio la funcion post CW-2

--- src/app/services/parking.service.ts
+++ src/app/services/parking.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Parking } from '../shared/parking';
@@ -24,11 +24,27 @@ export class ParkingService {
       )
     );
   }
+  public getParkingById(id: number): Observable<Parking> {
+    return this.executeQueryGet<Parking>('parkings', `/${id}`);
+  }
+  public createParking(parking: Parking): Observable<Parking> {
+    // product.id = 0;
+    return this.http
+      .post<Parking>(this.productsUrl, product, { headers: headers })
+      .pipe();
+  }
 
   private executeQueryGet<T>(endpoint: string, query?: string): Observable<T> {
     // todo: intentar refacotizar la query
     return this.http.get<T>(
       `${environment.apiUrlBase}/${endpoint}${(query ??= '')}`
     );
+  }
+  private executeQueryPost<T>(endpoint: string, newObj: string): Observable<T> {
+    // todo: intentar refacotizar la query
+    return this.http.post<T>(`${environment.apiUrlBase}`, newObj, {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 }

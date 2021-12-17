c2d0bad1a8e943e8bbbfdc2ba7ea35604c691955
Ikerls <ikerlarrea150@gmail.com>
2021-12-17 19:47:17 +0100

    feat: estructura basica del home

---
+++ src/app/shared/parking-data/parking-data.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-data',
+  templateUrl: './parking-data.component.html',
+  styleUrls: ['./parking-data.component.scss'],
+})
+export class ParkingDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

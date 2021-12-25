467a5628c43e0087dfb2229b7aeb3c5b3b67e00a
Ikerls <ikerlarrea150@gmail.com>
2021-12-25 23:22:27 +0100

    feat: Added quick actions component

---
+++ src/app/components/quick-actions/quick-actions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-actions',
+  templateUrl: './quick-actions.component.html',
+  styleUrls: ['./quick-actions.component.scss'],
+})
+export class QuickActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
+import { QuickActionsComponent } from '../../components/quick-actions/quick-actions.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DashboardPageRoutingModule
-  ],
-  declarations: [DashboardPage]
+  imports: [CommonModule, FormsModule, IonicModule, DashboardPageRoutingModule],
+  declarations: [DashboardPage, QuickActionsComponent],
 })
 export class DashboardPageModule {}

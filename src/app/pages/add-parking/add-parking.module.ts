a3b5564310706baf239bbc374f754731b212f787
Ikerls <ikerlarrea150@gmail.com>
2021-12-26 20:31:31 +0100

    feat: added new steps for the add-parking page

--- src/app/pages/add-parking/add-parking.module.ts
+++ src/app/pages/add-parking/add-parking.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AddParkingPageRoutingModule } from './add-parking-routing.module';
 
 import { AddParkingPage } from './add-parking.page';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AddParkingPageRoutingModule
+    AddParkingPageRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
   ],
-  declarations: [AddParkingPage]
+  declarations: [AddParkingPage],
 })
 export class AddParkingPageModule {}
--- src/app/pages/add-parking/add-parking.page.ts
+++ src/app/pages/add-parking/add-parking.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ParkingDataComponent } from '../../shared/parking-data/parking-data.component';
+import { ParkingService } from '../../core/parking.service';
+import { Parking } from '../../shared/parking';
 
 @Component({
   selector: 'app-add-parking',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-parking.page.scss'],
 })
 export class AddParkingPage implements OnInit {
+  constructor(private parkingService: ParkingService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addNewParking(e: any) {
+    // todo create new Parking
+    this.parkingService.getMaxParkingId().subscribe((id) => (e.id = ++id));
+    this.parkingService.createParking(e).subscribe(() => console.log('fd'));
+    this.parkingService
+      .getParkingById(5)
+      .subscribe((data) => console.log(data));
+    // console.log(e);
   }
-
 }

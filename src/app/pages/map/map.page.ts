1a66bba99ca8cd6a1f1af1cb273599d281d05e74
Ikerls <ikerlarrea150@gmail.com>
2022-01-07 12:26:43 +0100

    feat: added the main map

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-
-  constructor() { }
+  private map!: mapboxgl.Map;
+  constructor() {}
 
   ngOnInit() {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    this.map = new mapboxgl.Map({
+      container: 'map', //  containerID
+      style: 'mapbox://styles/fgergfer/ckx2flh2d12km14pcyrf8mfqw', // style URL
+      center: [-1.685128, 42.781851],
+      zoom: 17,
+    });
+    this.map.on('load', () => {
+      this.map.resize();
+    });
   }
-
 }

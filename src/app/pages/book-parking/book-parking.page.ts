bc7a536dcbe2aad1d4c8d4198a4d9c004c6cfc9f
Ikerls <ikerlarrea150@gmail.com>
2022-01-17 19:44:26 +0100

    refactor: changed the book parking view

--- src/app/pages/book-parking/book-parking.page.ts
+++ src/app/pages/book-parking/book-parking.page.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ParkingService } from '../../core/parking.service';
 import { Parking } from '../../shared/parking';
+import { share } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-book-parking',
@@ -12,6 +14,7 @@ export class BookParkingPage implements OnInit {
   @Input() id: number;
   parkingData: Parking;
   formBook: FormGroup;
+  type: string;
   constructor(
     private formBuilder: FormBuilder,
     private parkingService: ParkingService
@@ -19,34 +22,41 @@ export class BookParkingPage implements OnInit {
   ngOnInit(): void {
     this.parkingService.getParkingById(this.id).subscribe((data) => {
       this.parkingData = data;
+      this.type = data.type === 'larga estancia' ? 'Date' : 'time';
     });
     this.formBook = this.formBuilder.group({
-      periodsStrart: ['', Validators.required],
-      periodsEnd: ['', Validators.required],
+      periodStart: ['', Validators.required],
+      periodEnd: ['', Validators.required],
     });
   }
-  sendForm() {
-    let valido: boolean = true;
-    console.log(this.formBook.controls['periodsStrart'].value());
-    if (this.parkingData.type === 'larga estancia') {
-      valido = this.periodoValido(this.parkingData.daysAvailable);
+  sendForm(e: any) {
+    e.preventDefault();
+    if (this.formBook.valid) {
+      if (this.formBook.dirty) {
+        console.log('!');
+      }
     } else {
-      valido = this.periodoValido(this.parkingData.timesAvailable);
+      throwError('formulario no valido');
     }
+    // let valido: boolean = true;
+    // console.log(this.formBook.controls['periodsStrart'].value());
+    // if (this.parkingData.type === 'larga estancia') {
+    //   valido = this.periodoValido(this.parkingData.daysAvailable);
+    // } else {
+    //   valido = this.periodoValido(this.parkingData.timesAvailable);
+    // }
   }
   periodoValido(data?: { start: number; end: number }[]) {
-    data.forEach((periodo: { start: number; end: number }, index: number) => {
-      if (
-        this.formBook.controls['periodsStrart'].value() >= periodo.start &&
-        this.formBook.controls['periodsEnd'].value() <= periodo.end
-      ) {
-        return true;
-      }
-    });
-    return false;
+    // data.forEach((periodo: { start: number; end: number }, index: number) => {
+    //   if (
+    //     this.formBook.controls['periodsStrart'].value() >= periodo.start &&
+    //     this.formBook.controls['periodsEnd'].value() <= periodo.end
+    //   ) {
+    //     return true;
+    //   }
+    // });
+    // return false;
   }
-  typeParking() {
-    return this.parkingData.type === 'larga estancia' ? 'Date' : 'time';
-  }
+
   reserva() {}
 }

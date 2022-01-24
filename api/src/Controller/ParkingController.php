babbffd369d64efe2cb66299a6a99dd102b76966
unknown <migtarx@gmail.com>
2022-01-24 14:26:33 +0100

    feat: Added Parking controller for get request
    Added Parking Controller for requesting all database parkings | TODO: Dates and Hours ranges availables
    
    Co-authored-by: Alejandro Parra Purroy <alex.parra.purroy@gmail.com>
    Co-authored-by: migtarx <migtarx@gmail.com>

---
+++ api/src/Controller/ParkingController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Coordinates;
+use App\Entity\Parkings;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParkingController extends AbstractController
+{
+    /**
+     * @Route("/parkings", name="parking")
+     */
+    public function getParkings(): Response
+    {
+        $parkings = $this->getDoctrine()->getRepository(Parkings::class)->findAll();
+        $data = [];
+        foreach ($parkings as $parking){
+            $cords = [
+                "latitude"=>$parking->getCoordinates()->getLatitude(),
+                "longitude"=>$parking->getCoordinates()->getLongitude()
+            ];
+            $tmp = [
+                "id" => $parking->getId(),
+                "coordinates" => $cords,
+                "direction" => $parking->getDirection(),
+                "type" => $parking->getType(),
+                "staus" => $parking->getStatus(),
+                "price_per_hour" => $parking->getPricePerHour(),
+                "price_per_day" => $parking->getPricePerDay()
+            ];
+            $data[] = $tmp;
+        }
+
+        return $this->json([
+            'parkings' => $data
+        ]);
+    }
+}

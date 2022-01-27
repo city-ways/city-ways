fe3ca7d30ce0c8cda24b79973792a90426621b0d
unknown <migtarx@gmail.com>
2022-01-27 12:54:06 +0100

    feat: Adding access to timesAvailable property (needs fix)

--- api/src/Controller/ParkingController.php
+++ api/src/Controller/ParkingController.php
@@ -6,6 +6,7 @@ use App\Entity\Coordinates;
 use App\Entity\Parkings;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParkingController extends AbstractController
@@ -13,21 +14,28 @@ class ParkingController extends AbstractController
     /**
      * @Route("/parkings", name="parking")
      */
-    public function getParkings(): Response
+    public function getParkings(ManagerRegistry $doctrine): Response
     {
-        $parkings = $this->getDoctrine()->getRepository(Parkings::class)->findAll();
+        $entityManager = $doctrine->getManager();
+        $parkings = $entityManager->getRepository(Parkings::class)->findAll();
         $data = [];
+        $timesAvailableData = [];
         foreach ($parkings as $parking){
-            $cords = [
-                "latitude"=>$parking->getCoordinates()->getLatitude(),
-                "longitude"=>$parking->getCoordinates()->getLongitude()
-            ];
+            foreach ($parking->getTimesAvailable() as $timeRange){
+                $tmpTimes = [
+                    "start" => $timeRange->getTimeRanges()[0],
+                    "end" => $timeRange->getTimeRanges()[1]
+                ];
+                $timesAvailableData[] = $tmpTimes;
+            }
             $tmp = [
                 "id" => $parking->getId(),
-                "coordinates" => $cords,
+                "coordinates" => ["latitude"=>$parking->getCoordinates()->getLatitude(), "longitude"=>$parking->getCoordinates()->getLongitude()],
                 "direction" => $parking->getDirection(),
                 "type" => $parking->getType(),
                 "staus" => $parking->getStatus(),
+                "timesAvailable" => $timesAvailableData,
+                "daysAvailable" => $parking->getDatesAvailable(),
                 "price_per_hour" => $parking->getPricePerHour(),
                 "price_per_day" => $parking->getPricePerDay()
             ];

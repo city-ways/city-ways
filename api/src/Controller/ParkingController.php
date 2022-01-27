3ef714ba469c36e9863a896ebe39d32556772395
iker <ikerls@pm.me>
2022-01-27 12:02:48 +0100

    feat: added the method post for parkings endpoint

--- api/src/Controller/ParkingController.php
+++ api/src/Controller/ParkingController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ParkingController extends AbstractController
 {
     /**
-     * @Route("/parkings", name="parking")
+     * @Route("/parkings", name="parking", methods={"GET"})
      */
     public function getParkings(): Response
     {
@@ -36,6 +36,34 @@ class ParkingController extends AbstractController
 
         return $this->json([
             'parkings' => $data
+        ]);
+    }
+    /**
+     * @Route("/parkings", name="setParking", methods={"POST"})
+     */
+    public function setParking(): Response
+    {
+//        $parkings = $this->getDoctrine()->getRepository(Parkings::class)->findAll();
+//        $data = [];
+//        foreach ($parkings as $parking){
+//            $cords = [
+//                "latitude"=>$parking->getCoordinates()->getLatitude(),
+//                "longitude"=>$parking->getCoordinates()->getLongitude()
+//            ];
+//            $tmp = [
+//                "id" => $parking->getId(),
+//                "coordinates" => $cords,
+//                "direction" => $parking->getDirection(),
+//                "type" => $parking->getType(),
+//                "staus" => $parking->getStatus(),
+//                "price_per_hour" => $parking->getPricePerHour(),
+//                "price_per_day" => $parking->getPricePerDay()
+//            ];
+//            $data[] = $tmp;
+//        }
+//
+        return $this->json([
+            'parkings' => "create"
         ]);
     }
 }

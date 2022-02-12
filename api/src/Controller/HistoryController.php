b6033acc026c0d80e32e10a671bfbe30d1bde967
Ikerls <ikerlarrea150@gmail.com>
2022-02-12 10:48:36 +0100

    feat: added the logic of book a parking in backend

--- api/src/Controller/HistoryController.php
+++ api/src/Controller/HistoryController.php
@@ -22,12 +22,13 @@ class HistoryController extends AbstractController
         $this->validator = $validator;
     }
     /**
-     * @Route("/api/history/{userId}", name="userHistory", methods={"GET"}, requirements={"id": "\d+"})
+     * @Route("/api/history", name="userHistory", methods={"GET"})
      */
     public function userHistory(int $userId): Response
     {
         $entityManager = $this->doctrine->getManager();
-        $user = $entityManager->getRepository(Users::class)->find($userId);
+        $user = $this->getUser();
+//        $user = $entityManager->getRepository(Users::class)->find($userId);
         if (!$user) {
             return $this->json("No user found for id: $userId", 404);
         }

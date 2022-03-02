34b443f96e38d5c190328472c11a25459373afc8
Ikerls <ikerlarrea150@gmail.com>
2022-03-02 11:17:17 +0100

    refacor: delete user check

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -130,7 +130,12 @@ class UserController extends AbstractController
         }
         $this->denyAccessUnlessGranted("edit", $user);
         /** @var History $lastHistory */
-        $lastHistory = $user->getHistory()->first();
+        $lastHistory = null;
+        foreach ($user->getHistory() as $value) {
+            if ($value->isInProgress()) {
+                $lastHistory = $value;
+            }
+        }
         if ($lastHistory != null && $lastHistory->isInProgress()) {
             return $this->json("No se puede eliminar un usuario mientras esta ocupando un parking", 400);
         }
